8b1103e3d0eff10711fa298f8109a73d3702b52e
Christopher Kaster <ikasoki@gmail.com>
2014-06-27 17:36:16 +0200

    sort downloads by date and add no downloads page

--- js/download_manager.js
+++ js/download_manager.js
@@ -5,10 +5,18 @@ function refresh_download_list() {
 
     $("#dl-list").empty();
 
-    download_list.forEach(function(item) {
-        $("#dl-list").append("<a class='dl-item list-group-item' onclick='run_item(\"" +
-            item.name + "\", \"" + item.path + "\");'>" + item.name + "</a>");
-    });
+    if(download_list.length > 0) {
+        download_list.sort(function(item_a, item_b) {
+            return item_b.date > item_a.date;
+        })
+
+        download_list.forEach(function(item) {
+            $("#dl-list").append("<a class='dl-item list-group-item' onclick='run_item(\"" +
+                item.name + "\", \"" + item.path + "\");'>" + item.name + "</a>");
+        });
+    } else {
+        $("#dl-list").append("<a class='list-group-item list-group-item-warning'>No downloads found.</a>");
+    }
 }
 
 function run_item(name, path) {
