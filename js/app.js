f72d880756dc8ce26979f4dc9add693518ae64ab
Christopher Kaster <ikasoki@gmail.com>
2014-06-27 16:18:32 +0200

    add ability to display downloads count to the download badge

--- js/app.js
+++ js/app.js
@@ -24,89 +24,145 @@ var http = require("http");
 var fs = require("fs");
 var path = require("path");
 
+var DOWNLOAD_LIST_IDENT = "NYAA_DOWNLOAD_LIST_IDENT";
+
 var search_options = {
-	base_url: URL_NYAA,
-	query: undefined
+    base_url: URL_NYAA,
+    query: undefined
 };
 
 $("#search-field").keydown(function(e) {
-	if(e.keyCode == 13) {
-		e.preventDefault();
-		$("#search-btn").click();
-		return true;
-	}
+    if(e.keyCode == 13) {
+        e.preventDefault();
+        $("#search-btn").click();
+        return true;
+    }
 });
 
 $("#search-btn").click(function() {
-	var raw_tags = $("#search-field").val();
-	
-	var tags = raw_tags.split(' ');
-	
-	search_options.query = tags;
-	
-	refresh();
+    var raw_tags = $("#search-field").val();
+
+    var tags = raw_tags.split(' ');
+
+    search_options.query = tags;
+
+    refresh();
 });
 
 $(document).ready(function() {
-	refresh();
+    refresh_download_list();
+    refresh();
 });
 
 function refresh() {
-	nyaa_fetch(0, search_options, function(items) {
-		$("#tlist").empty();
-		items.forEach(insert_list_item);
-	});
+    nyaa_fetch(0, search_options, function(items) {
+        $("#tlist").empty();
+        items.forEach(insert_list_item);
+    });
 }
 
 function insert_list_item(item) {
-	var item_mod = "";
-	
-	if(item.aplus) {
-		item_mod = " list-group-item-info";
-	} else if(item.remake) {
-		item_mod = " list-group-item-warning";
-	} else if(item.trusted) {
-		item_mod = " list-group-item-success";
-	}
-	
-	if(item.seeds == 0) {
-		item_mod = " list-group-item-danger";
-	}
-	
-	$("#tlist").append("<a class='dl-item list-group-item" + item_mod + "' onclick='download_item(\"" + item.download_link +
-		"\");'>" + item.title + "<div class='pull-right dl-info'>" + 
-			"<span class='glyphicon glyphicon-arrow-up'></span> " + pad(item.seeds, 4) + 
-			" <span class='glyphicon glyphicon-arrow-down'></span> " + pad(item.leech, 4) + 
-			" <span class='glyphicon glyphicon-hdd'></span> " + item.size +
-			"</div></a>");
+    var item_mod = "";
+
+    if(item.aplus) {
+        item_mod = " list-group-item-info";
+    } else if(item.remake) {
+        item_mod = " list-group-item-warning";
+    } else if(item.trusted) {
+        item_mod = " list-group-item-success";
+    }
+
+    if(item.seeds == 0) {
+        item_mod = " list-group-item-danger";
+    }
+
+    $("#tlist").append("<a class='dl-item list-group-item" + item_mod + "' onclick='download_item(\"" +
+        item.title + "\", \"" + item.download_link + "\");'>" + item.title + "<div class='pull-right dl-info'>" +
+        "<span class='glyphicon glyphicon-arrow-up'></span> " + pad(item.seeds, 4) +
+        " <span class='glyphicon glyphicon-arrow-down'></span> " + pad(item.leech, 4) +
+        " <span class='glyphicon glyphicon-hdd'></span> " + item.size +
+        "</div></a>");
 }
 
-function download_item(url) {
-	fs.mkdir("downloads", function() {
-		var filename_raw = url.split("tid=");
-		
-		var filename = filename_raw[filename_raw.length - 1] + ".torrent";
-		
-		var file_path = path.join("downloads", filename);
-	
-		var file = fs.createWriteStream(filename);
+function download_item(name, url) {
+    var download_path = path.join(get_home_dir(), "Downloads", "nyaa-explorer");
 
-		var request = http.get(url, function(response) {
-			response.pipe(file);
-			
-			response.on("end", function() {
-				gui.Shell.openItem(filename);
-				
-				setTimeout(function() {
-					fs.rename(filename, file_path);
-				}, 5000);
-			});
-		});
-	});
+    fs.mkdir(download_path, function() {
+        var filename_without_ext = name.split(' ').join('_');
+
+        var filename = filename_without_ext + ".torrent";
+
+        var file_path = path.join(download_path, filename);
+
+        var file = fs.createWriteStream(file_path);
+
+        var request = http.get(url, function(response) {
+            response.pipe(file);
+
+            response.on("end", function() {
+                add_to_download_list({
+                    name: filename,
+                    path: file_path,
+                    date: new Date()
+                });
+            });
+        });
+    });
+}
+
+function get_download_list() {
+    var download_list_json = window.localStorage.getItem(DOWNLOAD_LIST_IDENT);
+
+    if(!download_list_json) {
+        return [];
+    }
+
+    return JSON.parse(download_list_json);
+}
+
+function add_to_download_list(item) {
+    var download_list = get_download_list();
+
+    var already_downloaded = false;
+
+    download_list.forEach(function(element) {
+        if(item.name == element.name) {
+            console.log("item already downloaded, push date");
+            item.date = new Date();
+
+            already_downloaded = true;
+        }
+    });
+
+    if(!already_downloaded) {
+        console.log("new item in download list:\n" + JSON.stringify(item, 4));
+
+        download_list.push(item);
+    }
+
+    window.localStorage.setItem(DOWNLOAD_LIST_IDENT, JSON.stringify(download_list));
+
+    refresh_download_list();
+}
+
+function clear_downloads() {
+    window.localStorage.setItem(DOWNLOAD_LIST_IDENT, JSON.stringify([]));
+
+    refresh_download_list();
+}
+
+function refresh_download_list() {
+    var download_list = get_download_list();
+
+    $(".download-badge").html(download_list.length);
 }
 
 function pad(num, size) {
-	var s = num+"";
-	while (s.length < size) s = "0" + s;
-	return s;
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
+function get_home_dir() {
+    return process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
 }
--- js/nyaa.js
+++ js/nyaa.js
@@ -23,76 +23,76 @@ URL_NYAA = "http://www.nyaa.se/?page=rss";
 URL_NYAA_SUKEBEI = "http://sukebei.nyaa.se/?page=rss";
 
 function nyaa_fetch(page, search_info, callback) {
-	var url = _nyaa_create_url(page, search_info);
-	
-	$.get(url, function(data) {
-		var xml = $(data);
-		
-		var items = [];
-		
-		$(xml).find("item").each(function() {
-			// looks like this: x leecher(s), y seeder(s), z download(s) - SIZE - Trusted
-			var description = $(this).find("description").text();
-		
-			// split it open to get leechers, seeders and download + more in 3 entries
-			var desc_raw = description.split(", ");
+    var url = _nyaa_create_url(page, search_info);
 
-			// retrieve seeds and leecher info
-			var seeds = desc_raw[0].split(' ')[0];
-			var leech = desc_raw[1].split(' ')[0];
-			
-			// split downloads + more info
-			var tmp = desc_raw[2].split(' - ');
-			
-			var downloads = tmp[0].split(' ')[0];
-			var size = tmp[1];
-			
-			var trusted = false;
-			var aplus = false;
-			var remake = false;
-			
-			// A+ - Trusted
-			if(tmp.length > 2) {
-				var mods = tmp.slice(2);
-				
-				trusted = mods.indexOf("Trusted") > -1;
-				aplus = mods.indexOf("A+") > -1;
-				remake = mods.indexOf("Remake") > -1;
-			}
-		
-			var item = {
-				title: $(this).find("title").text(),
-				category: $(this).find("category").text(),
-				download_link: $(this).find("link").text(),
-				page_link: $(this).find("guid").text(),
-				seeds: seeds,
-				leech: leech,
-				downloads: downloads,
-				size: size,
-				trusted: trusted,
-				aplus: aplus,
-				remake: remake
-			};
+    $.get(url, function(data) {
+        var xml = $(data);
 
-			items.push(item);
-		});
-		
-		callback(items);
-	});
+        var items = [];
+
+        $(xml).find("item").each(function() {
+            // looks like this: x leecher(s), y seeder(s), z download(s) - SIZE - Trusted
+            var description = $(this).find("description").text();
+
+            // split it open to get leechers, seeders and download + more in 3 entries
+            var desc_raw = description.split(", ");
+
+            // retrieve seeds and leecher info
+            var seeds = desc_raw[0].split(' ')[0];
+            var leech = desc_raw[1].split(' ')[0];
+
+            // split downloads + more info
+            var tmp = desc_raw[2].split(' - ');
+
+            var downloads = tmp[0].split(' ')[0];
+            var size = tmp[1];
+
+            var trusted = false;
+            var aplus = false;
+            var remake = false;
+
+            // A+ - Trusted
+            if(tmp.length > 2) {
+                var mods = tmp.slice(2);
+
+                trusted = mods.indexOf("Trusted") > -1;
+                aplus = mods.indexOf("A+") > -1;
+                remake = mods.indexOf("Remake") > -1;
+            }
+
+            var item = {
+                title: $(this).find("title").text(),
+                category: $(this).find("category").text(),
+                download_link: $(this).find("link").text(),
+                page_link: $(this).find("guid").text(),
+                seeds: seeds,
+                leech: leech,
+                downloads: downloads,
+                size: size,
+                trusted: trusted,
+                aplus: aplus,
+                remake: remake
+            };
+
+            items.push(item);
+        });
+
+        callback(items);
+    });
 }
 
 function _nyaa_create_url(page, search_info) {
-	if(!search_info) {
-		search_info = {};
-	}
-	
-	var url = search_info.base_url ? search_info.base_url : URL_NYAA;
-	
-	url += page ? "&offset=" + page : "";
-	
-	// TODO: add category, filter, sort and order options
-	
-	url += search_info.query ? "&term=" + search_info.query.join("+") : "";
-	
-	return url;
+    if(!search_info) {
+        search_info = {};
+    }
+
+    var url = search_info.base_url ? search_info.base_url : URL_NYAA;
+
+    url += page ? "&offset=" + page : "";
+
+    // TODO: add category, filter, sort and order options
+
+    url += search_info.query ? "&term=" + search_info.query.join("+") : "";
+
+    return url;
 }
